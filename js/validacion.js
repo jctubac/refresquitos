23252d6a0b335f1e32eb4e788daf6d8d8a4840d7
jctubac <91978222+jctubac@users.noreply.github.com>
2021-10-05 14:13:40 -0600

    Add files via upload

---
+++ js/validacion.js
@@ -0,0 +1,15 @@
+function go() {
+    if(document.miformulario.Usuario.value=='joceline' &&  document.miformulario.Contraseña.value=='examen'){
+    document.miformulario.submit();
+}
+
+
+else{
+    alert("ingrese sus usuario y contraseña correctamente.");
+}
+
+
+
+
+
+}
